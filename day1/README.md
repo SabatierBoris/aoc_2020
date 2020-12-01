912a406abccb8fe52a5d6a153b8f1c8928c7db67
Boris Sabatier <boris@sabatier.io>
2020-12-01 18:41:04 +0100

    Part 2

--- day1/README.md
+++ day1/README.md
@@ -3,3 +3,11 @@
 ## Partie 1
 J'utilise un Set pour se souvenir de tous les nombres déjà lu.
 Je calcule la différence attendue, et si elle est présente dans le Set : Gagné !
+
+## Partie 2
+J'ai toujours un Set pour l'ensemble des nombres déjà parcouru.
+A chaque nouveau nombre, je parcourt le Set et je calcule la somme et le produit.
+Et j'ajoute ça dans une HashMap avec comme clé la somme et valeur le produit.
+
+Bien sur, avant ça, je regarde si la différence attendue est une clé du la HashMap.
+Si c'est le cas, j'utilise la valeur pour faire le produit final.
---
+++ day1/src/bin/part2.rs
@@ -0,0 +1,30 @@
+extern crate day1;
+
+use std::collections::HashSet;
+use std::collections::HashMap;
+
+fn main() {
+    let mut numbers = HashSet::<i32>::new();
+    let mut tmp_sum = HashMap::<i32, i32>::new();
+    loop {
+        match day1::read_number() {
+            Ok(num) => {
+                let diff = 2020 - num;
+                if tmp_sum.contains_key(&diff) {
+                    let val = tmp_sum.get(&diff).unwrap();
+                    println!("result = {}", num*val);
+                    break;
+                } else {
+                    for tmp in &numbers {
+                        tmp_sum.insert(num+tmp, tmp*num);
+                    }
+                    numbers.insert(num);
+                };
+            }
+            Err(error) => {
+                println!("{:?}", error);
+                break;
+            }
+        }
+    }
+}
